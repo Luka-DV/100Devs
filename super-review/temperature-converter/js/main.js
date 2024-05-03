076ba85f554122affcd81e66b0c0c6954bcaee82
Luka-DV <luka.guva@gmail.com>
2024-05-03 10:40:48 +0200

    add Temp converter code

--- super-review/temperature-converter/js/main.js
+++ super-review/temperature-converter/js/main.js
@@ -1,1 +1,17 @@
 //Write your pseduo code first! 
+
+//need the value in Cels
+//convert the value to Fahernheit
+//return the value in F.
+
+
+document.querySelector("button").addEventListener("click", convertTemp);
+
+function convertTemp() {
+
+    const tempInCels = Number(document.querySelector("#value").value);
+
+    const tempInFahren = tempInCels * 1.8 + 32;
+
+    document.querySelector("#temp").innerText = tempInFahren;
+}
