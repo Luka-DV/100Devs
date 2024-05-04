cc92a4d88cfdf27bf2731a804574e0332434a16f
Luka-DV <luka.guva@gmail.com>
2024-05-04 11:31:05 +0200

    add solution to bachelor code 3

--- super-review/bachelor-code/3/js/main.js
+++ super-review/bachelor-code/3/js/main.js
@@ -1,11 +1,39 @@
-const contestants = document.querySelectorAll(/*INSERTCODE*/)
+/* const contestants = document.querySelectorAll(".contestant");
 
-Array.from(contestants).forEach(element => element.addEventListener('click', /*INSERTCODE*/))
+Array.from(contestants).forEach(element => element.addEventListener('click', checkForRose));
 
 function checkForRose(click){
-	if(click.target.classList.contains(/*INSERTCODE*/)){
-		document.querySelector('#nikki').classList.toggle(/*INSERTCODE*/)
+	if(click.target.classList.contains("rose")){
+		document.querySelector('#nikki').classList.toggle("hidden");
+	}else{
+		alert("Wrong!");
+	}
+} */
+
+
+//you dont need the Array.from() method anymore:
+
+//"In modern browsers, NodeList objects support the forEach method, so there's no need to convert it to an array explicitly.""
+
+
+const contestants = document.querySelectorAll(".contestant"); //gives a NodeList
+
+console.log(contestants);
+console.log(contestants.entries());
+const entriesArray = Array.from(contestants);
+console.log(("array", entriesArray));
+console.log("keys", entriesArray.values())
+console.log(entriesArray.entries());
+
+contestants.forEach(element => element.addEventListener('click', checkForRose));
+
+function checkForRose(click){
+	console.log("click: ",click)
+	console.log("click target: ", click.target)
+	if(click.target.classList.contains("rose")){
+		document.querySelector('#nikki').classList.toggle("hidden");
 	}else{
 		alert("Wrong!");
 	}
 }
+
