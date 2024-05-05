5ab11b70f9d8aafde2569b96a9179ea51ef757f4
Luka-DV <luka.guva@gmail.com>
2024-05-05 15:27:48 +0200

    add solution to sir savage exercise

--- super-review/sir-savage-the-21st/js/main.js
+++ super-review/sir-savage-the-21st/js/main.js
@@ -1,2 +1,27 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
 //Bonus can you make it print '21' 21 times to the dom?
+
+/* function savageSays(num) {
+
+    for(let i = 1; i <= num; i++) {
+        console.log("21");
+        document.querySelector("#savageSays").innerText += ` ${num}`;
+    }
+}
+
+savageSays(21); */
+
+
+function savageSays(num) {
+
+    for(let i = 1; i <= num; i++) {
+        console.log(num);
+
+        const newElement = document.createElement("h2");
+        newElement.innerText += `${num}`;
+        document.body.appendChild(newElement);
+    }
+}
+
+savageSays(21);
+
