49d07a31dfe5979cc1e61509db8ff2aa3299fb8d
Luka-DV <luka.guva@gmail.com>
2024-05-10 16:08:23 +0200

    add obj listen to your heart solution

--- super-review/objects-listen-to-your-heart/js/main.js
+++ super-review/objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,27 @@
 //Create a stopwatch object that has four properties and three methods
+
+
+const stopwatch = {};
+
+stopwatch.color = "black";
+stopwatch["shape"] = "round";
+stopwatch.brand = "nike";
+stopwatch["number of functions"] = 6;
+
+stopwatch.startTimer = function() {
+    console.log("stopwatch timer started");
+    stopwatch.startTime = new Date().getTime();
+}
+
+stopwatch.stopTimer = function() {
+    console.log("timer stopped");
+    stopwatch.stopTime = new Date().getTime();
+}
+
+stopwatch.showTime = function() {
+    console.log("time elapsed: ");
+    console.log((stopwatch.stopTime - stopwatch.startTime)/1000, "seconds");
+    console.log(this.color);
+}
+
+console.log(stopwatch);
