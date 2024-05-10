223e388d849c838dceb98132b9569f839114effa
Luka-DV <luka.guva@gmail.com>
2024-05-10 12:14:37 +0200

    add more examples to review in paradise

--- super-review/review-in-paradise-after-paradise/js/main.js
+++ super-review/review-in-paradise-after-paradise/js/main.js
@@ -9,5 +9,46 @@ function checkArray(arr) {
     } else alert("We close in an hour")
 }
 
-checkArray([140,1,22])
+//checkArray([140,1,22])
 
+
+const arr = new Array(1, 2, 3, 4, 5, 66, 7);
+console.log(arr);
+
+const findBiggestNum = arr => {
+
+    //let biggestNum = -Infinity;
+
+   /*  for(let num of arr ) {
+        if(num > biggestNum) {
+            biggestNum = num;
+        }
+    } */
+
+   /*  arr.forEach(num => {
+        if(num > biggestNum) {
+            biggestNum = num;
+        }
+    }) */
+
+  /*   const biggestNum = arr.reduce((acc, crr) => {
+        if(crr > acc) {
+            acc = crr;
+        }
+        return acc;
+    }, -Infinity) */
+
+
+    const biggestNum = Math.max(...arr);
+
+
+    console.log(biggestNum);
+}
+
+findBiggestNum(arr);
+
+let cars = ['Honda', 'Toyota', 'Ford', 'Tesla']
+const nums = [1,2,3]
+cars = nums
+nums[0] = 99;
+console.log( cars ) //[99,2,3]
